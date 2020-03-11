978771d2d1fb275ad536e14c7129bab9774d89c9
caltaylor101 <34333250+caltaylor101@users.noreply.github.com>
2020-03-11 14:32:11 -0600

    End of Section 11

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -43,7 +43,7 @@ namespace DatingApp.API
             });
             services.AddCors();
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
-
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
 
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>(); 
