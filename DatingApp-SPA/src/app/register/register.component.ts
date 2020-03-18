3704df442dbf51895fc688276bddda91f9258ba8
caltaylor101 <34333250+caltaylor101@users.noreply.github.com>
2020-03-18 13:39:41 -0600

    For TZ Transfer

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../_services/auth.service';
 import { EventEmitter } from '@angular/core';
 import { AlertifyService } from '../_services/alertify.service';
 import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,17 +13,28 @@ import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
   constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+      this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(15)]),
+      confirmPassword: new FormControl('', Validators.required)
+    }, this.passwordMatchValidator);
+  }
+
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true};
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alertify.success('registration successful');
-    }, error => {
-      this.alertify.error(error);
-    });
+    // this.authService.register(this.model).subscribe(() => {
+    //   this.alertify.success('registration successful');
+    // }, error => {
+    //   this.alertify.error(error);
+    // });
 
   }
   cancel() {
